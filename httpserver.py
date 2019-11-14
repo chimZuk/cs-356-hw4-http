b807f082e793a964d072a3ba93e70c9672233fa0
DZMITRY KUZMITCH <kuzya19989@outlook.com>
2019-11-14 17:25:32 -0500

    Added new code for the server

--- httpserver.py
+++ httpserver.py
@@ -1,25 +1,32 @@
 #! /usr/bin/env python3
-# Dzmitry Kuzmitch 31479051 Section 101
+# TCP Echo Server
 
 import sys
 import socket
-import struct
-import time
-import random
 
 serverIP = sys.argv[1]
 serverPort = int(sys.argv[2])
-
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+dataLen = 1000000
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind((serverIP, serverPort))
+serverSocket.listen(1)
 
-print()
-print("The server is ready to receive on port: " + str(serverPort) + "\n")
+response = "HTTP/1.1 200 OK\r\n"
+response += "Date: Sun, 04 Mar 2018 21:24:58 GMT\r\n"
+response += "Last-Modified: Fri, 02 Mar 2018 21:06:02 GMT\r\n"
+response += "Content-Length: 75\r\n"
+response += "Content-Type: text/html; charset=UTF-8\r\n"
+response += "\r\n"
+response += "<html><p>First Line<br />Second Line<br />Third Line<br />COMPLETE<p><html>"
 
-count = 0
+print(response)
 
-while count < 2:
-    packed_data, address = serverSocket.recvfrom(1024)
-    print(packed_data)
-    serverSocket.sendto("LOL", address)
-    count = count + 1
+print('The server is ready to receive on port:  ' + str(serverPort) + '\n')
+
+while True:
+    connectionSocket, address = serverSocket.accept()
+    print("Socket created for client " + address[0] + ", " + str(address[1]))
+    data = connectionSocket.recv(dataLen).decode()
+    connectionSocket.send(response.encode())
+
+        
