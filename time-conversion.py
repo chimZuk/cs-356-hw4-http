4010f005d22b6f48e3bf4c5410cbd68106deee40
DZMITRY KUZMITCH <kuzya19989@outlook.com>
2019-11-14 02:57:02 -0500

    Finished time conversion

--- time-conversion.py
+++ time-conversion.py
@@ -1,28 +1,29 @@
 #Get current time in UTC/GMT time zone and convert to string in HTTP format:
 
 import datetime, time
-t = datetime.datetime.now(timezone.utc)
-date = time.strftime("%a, %d %b %Y %H:%M:%S %Z\r\n", t)
-
+tn = datetime.datetime.now(datetime.timezone.utc)
+date = tn.strftime("%a, %d %b %Y %H:%M:%S %Z\r\n")
+print("Date now: " + date)
 
 #Determining a file’s modification time (in seconds since 1 Jan, 1970 on Unix machines)
 
 import os.path
+filename = "filename.html"
 secs = os.path.getmtime(filename)
 
 
 #Convert above time to UTC /GMT (returns a time tuple):
 
-import time
-t = time.gmtime(secs)
+tg = time.gmtime(secs)
 
 
 #Convert above time tuple to a string in HTTP format:
 
-last_mod_time = time.strftime("%a, %d %b %Y %H:%M:%S GMT\r\n", t)
+last_mod_time = time.strftime("%a, %d %b %Y %H:%M:%S GMT\r\n", tg)
+print("Modified at (HTTP): " + last_mod_time)
 
 
 #Convert a date/time in string format back to time tuple and seconds since 1 Jan, 1970
 
-t = time.strptime(last_mod_time, "%a, %d %b %Y %H:%M:%S %Z\r\n")
-secs = time.mktime(t)
+tg = time.strptime(last_mod_time, "%a, %d %b %Y %H:%M:%S %Z\r\n")
+secs = time.mktime(tg)
